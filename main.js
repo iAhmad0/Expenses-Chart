5a412c0be2c1c1d27027fc65f69f98f56b70c8c2
Ahmad Hussein <92392121+iAhmad0@users.noreply.github.com>
2023-09-02 15:56:17 +0300

    added comments to main.js file

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// Obtaining today's date and determining the day
 const date = new Date();
 const getDay = date.toDateString().slice(0, 3);
 
 
+// Fetching the data from the json file
 const getData = async () => {
     const response = await fetch('./data.json');
     const info = await response.json();
@@ -11,23 +13,31 @@ const getData = async () => {
     specifyToday();
 }
 
+
+// Calculating and displaying the height of the bars compared to the biggest amount bar
 function determineHeight(obj) {
-    const greater = obj.reduce((a, b) => Math.max(a, b.amount), Number.NEGATIVE_INFINITY);
+    const biggest = obj.reduce((a, b) => Math.max(a, b.amount), Number.NEGATIVE_INFINITY);
     obj.forEach((day) => {
-        const height = day.amount*160 / greater;
+        const height = day.amount*160 / biggest; // assuming the height of the biggest bar to be 160px
         document.querySelector(`.${day.day} .bar`).style.height = `${height}px`;
     });
 }
 
+
+// Display the amount of each bar
 function displayAmount(obj) {
     obj.forEach((day) => {
         document.querySelector(`.${day.day} .amount`).textContent = "$" + day.amount;
     });
 }
 
+
+// Highlight the bar of today's date
 function specifyToday() {
     const today = getDay.toLowerCase();
     document.querySelector(`.${today} .bar`).classList.add('today');
 }
 
+
+// Calling the function
 getData();
